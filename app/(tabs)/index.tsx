8bf2c3086f591bbf2e318e9f1fb8909588379f7c
Mitch-creates <mitchcreates@proton.me>
2025-07-21 22:35:20 +0200

    Implemented basic fetching from TMDB

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,25 @@
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Image, ScrollView, View, Text } from 'react-native';
 
 import '../../global.css';
+import { images } from 'constants/images';
+import { icons } from 'constants/icons';
+import { useRouter } from 'expo-router';
+import SearchBar from '../../components/SearchBar';
 
-export default function App() {
+export default function Index() {
+  const router = useRouter();
   return (
-    <>
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-bold text-2xl text-accent">Hello, world!</Text>
-        <Text className="text-gray-500">This is a NativeWind example.</Text>
-        <Link href="/onboarding">Onboarding</Link>
-        <Link href="/movie/123">Spiderman</Link>
-      </View>
-    </>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute z-0 w-full" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
+        <Image source={icons.logo} className="mx-auto mb-5 mt-20 h-10 w-12" />
+        <View className="mt-5 flex-1">
+          <SearchBar onPress={() => router.push('/search')} placeholder="Search for a movie" />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { View, Text, Image, TextInput } from 'react-native';
+import { icons } from 'constants/icons';
+
+interface SearchBarProps {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: SearchBarProps) => {
+  return (
+    <View className="flex-row items-center rounded-full bg-dark-200 px-5 py-4">
+      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="ml-2 flex-1 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
