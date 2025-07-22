d9b3d165bafaec475f5e4f2d859fe38de6ec5739
Mitch-creates <mitchcreates@proton.me>
2025-07-22 11:56:04 +0200

    Implemented search functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,17 +4,19 @@ import { icons } from 'constants/icons';
 interface SearchBarProps {
   onPress?: () => void;
   placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ onPress, placeholder }: SearchBarProps) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText }: SearchBarProps) => {
   return (
     <View className="flex-row items-center rounded-full bg-dark-200 px-5 py-4">
       <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#a8b5db"
         className="ml-2 flex-1 text-white"
       />
